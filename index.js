36b3b84dbb3b93910452e2e8eb942bf494077bb4
Vladislav Romashko <vadimromashko@gmail.com>
2022-06-22 23:39:35 +0300

    one more commit

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 console.log('Hello World')
 
-<<<<<<< HEAD
 const userAge = 35
 
 if (userAge >= 18) {
-=======
-const age = 20
-if (age >= 18) {
->>>>>>> feature/user-access
   console.log('Вы совершенолетний')
 } else {
   console.log('Вы не совершенолетний')
